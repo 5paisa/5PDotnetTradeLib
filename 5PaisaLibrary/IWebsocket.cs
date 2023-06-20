efe1b4cd1c8b295d4cb9d5925eca1aee92d1ad0b
5paisa <65898074+5paisa@users.noreply.github.com>
2023-06-20 16:24:20 +0530

    Add files via upload

---
+++ 5PaisaLibrary/IWebsocket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5PaisaLibrary
+{
+    internal interface IWebsocket
+    {
+       
+        void Connect(string jwttoken, string clientcode);
+       
+       
+   
+    }
+}
